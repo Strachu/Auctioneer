c53559f1e68b554f01066a1f44b0ead92d0d780b
Patryk Strach <P.Strachu@gmail.com>
2015-07-27 11:18:27 +0200

    Simplify passing of query strings a bit by creating a function which automatically copies current parameters from URL

--- Sources/Auctioneer.Presentation/Helpers/HtmlHelper.Extensions.cs
+++ Sources/Auctioneer.Presentation/Helpers/HtmlHelper.Extensions.cs
@@ -26,5 +26,30 @@ namespace Auctioneer.Presentation.Helpers
 
 			return helper.ActionLink(linkText, actionName, controllerName, routeDictionary, htmlAttibutesDictionary);
 		}
+
+		public static IHtmlString ActionLinkWithCurrentParameters(this HtmlHelper helper,
+		                                                          string linkText,
+		                                                          string actionName,
+		                                                          string controllerName = null,
+		                                                          object routeValues = null,
+		                                                          object htmlAttributes = null)
+		{
+			var currentParameters       = HttpContext.Current.Request.QueryString;
+			var routeDictionary         = new RouteValueDictionary();
+			var routeOverrides          = new RouteValueDictionary(routeValues);
+			var htmlAttibutesDictionary = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+
+			foreach(var parameter in currentParameters.AllKeys)
+			{
+				routeDictionary[parameter] = currentParameters[parameter];
+			}
+
+			foreach(var routeOverride in routeOverrides)
+			{
+				routeDictionary[routeOverride.Key] = routeOverride.Value;
+			}
+
+			return helper.ActionLink(linkText, actionName, controllerName, routeDictionary, htmlAttibutesDictionary);
+		}
 	}
 }
--- Sources/Auctioneer.Presentation/Helpers/UrlHelper.Extensions.cs
+++ Sources/Auctioneer.Presentation/Helpers/UrlHelper.Extensions.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace Auctioneer.Presentation.Helpers
 {
@@ -14,6 +15,28 @@ namespace Auctioneer.Presentation.Helpers
 			var fileName = auctionId + ".jpg";
 
 			return url.Content("~/Content/Auctions/Thumbnails/" + fileName);
+		}
+
+		public static string ActionWithCurrentParameters(this UrlHelper helper,
+		                                                 string actionName,
+		                                                 string controllerName = null,
+		                                                 object routeValues = null)
+		{
+			var currentParameters = HttpContext.Current.Request.QueryString;
+			var routeDictionary   = new RouteValueDictionary();
+			var routeOverrides    = new RouteValueDictionary(routeValues);
+
+			foreach(var parameter in currentParameters.AllKeys)
+			{
+				routeDictionary[parameter] = currentParameters[parameter];
+			}
+
+			foreach(var routeOverride in routeOverrides)
+			{
+				routeDictionary[routeOverride.Key] = routeOverride.Value;
+			}
+
+			return helper.Action(actionName, controllerName, routeDictionary);
 		}
 	}
 }
