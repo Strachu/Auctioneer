c9e8840eef2f36844ece5b2f19bc97698e16b65f
Patryk Strach <P.Strachu@gmail.com>
2015-07-30 19:40:33 +0200

    Split site.css into multiple files

--- Sources/Auctioneer.Presentation/App_Start/BundleConfig.cs
+++ Sources/Auctioneer.Presentation/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace Auctioneer.Presentation
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 						 "~/Content/bootstrap.css",
-						 "~/Content/site.css"));
+						 "~/Content/Site_CSS/*.css"));
 		}
 	}
 }
