f353e92d8fe2d47cf3f9f6213d1ae51e8e1f5930
Patryk Strach <P.Strachu@gmail.com>
2015-07-31 10:34:09 +0200

    Forgot to remove the prefix from resource strings in AuctionAddViewModel

--- Sources/Auctioneer.Presentation/Models/Auction/AuctionAddViewModel.cs
+++ Sources/Auctioneer.Presentation/Models/Auction/AuctionAddViewModel.cs
@@ -25,36 +25,36 @@ namespace Auctioneer.Presentation.Models
 		[Required]
 		[DataType(DataType.Text)]
 		[StringLength(50, MinimumLength = 5)]
-		[Display(Name = "AddAuction_Title", ResourceType = typeof(Lang))]
+		[Display(Name = "Title", ResourceType = typeof(Lang))]
 		public string Title { get; set; }
 
 		[Required]
 		[Range(1, 14)]
-		[Display(Name = "AddAuction_DaysToEnd", ResourceType = typeof(Lang))]
+		[Display(Name = "DaysToEnd", ResourceType = typeof(Lang))]
 		public int DaysToEnd { get; set; }
 		public SelectList AvailableDaysToEnd { get; set; }
 
 		[Required]
 		[Min(1.0)]
 		[DataType(DataType.Currency)]
-		[Display(Name = "AddAuction_Price", ResourceType = typeof(Lang))]
+		[Display(Name = "Price", ResourceType = typeof(Lang))]
 		public decimal? Price { get; set; }
 
 		[Required]
-		[Display(Name = "AddAuction_Category", ResourceType = typeof(Lang))]
+		[Display(Name = "Category", ResourceType = typeof(Lang))]
 		public int CategoryId { get; set; }
 		public IEnumerable<SelectListItem> AvailableCategories { get; set; }
 
 		[Required]
 		[ContentType("image/jpeg", // TODO the better solution would be to accept any image and convert it to a JPEG automatically
-			ErrorMessageResourceName = "AddAuction_OnlyJPEGImagesSupported", ErrorMessageResourceType = typeof(Lang))]
-		[Display(Name = "AddAuction_Photos", ResourceType = typeof(Lang))]
+			ErrorMessageResourceName = "OnlyJPEGImagesSupported", ErrorMessageResourceType = typeof(Lang))]
+		[Display(Name = "Photos", ResourceType = typeof(Lang))]
 		public IEnumerable<HttpPostedFileBase> Photos { get; set; }
 
 		[AllowHtml]
 		[DataType(DataType.Html)]
-		[Required(ErrorMessageResourceName = "AddAuction_DescriptionRequired", ErrorMessageResourceType = typeof(Lang))]
-		[Display(Name = "AddAuction_Description", ResourceType = typeof(Lang))]
+		[Required(ErrorMessageResourceName = "DescriptionRequired", ErrorMessageResourceType = typeof(Lang))]
+		[Display(Name = "Description", ResourceType = typeof(Lang))]
 		public string Description { get; set; }
 	}
 }
