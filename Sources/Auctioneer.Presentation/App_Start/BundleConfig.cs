76d26e063d7350bb2901304536627add71cb9064
Patryk Strach <P.Strachu@gmail.com>
2015-08-07 13:14:31 +0200

    Small directory css and scripts directory reorganization

--- Sources/Auctioneer.Presentation/App_Start/BundleConfig.cs
+++ Sources/Auctioneer.Presentation/App_Start/BundleConfig.cs
@@ -9,23 +9,23 @@ namespace Auctioneer.Presentation
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-							"~/Scripts/jquery-{version}.js"));
+							"~/Content/Libraries/JQuery/jquery-{version}.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-							"~/Scripts/jquery.validate*"));
+							"~/Content/Libraries/JQuery/jquery.validate*"));
 
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-							"~/Scripts/modernizr-*"));
+							"~/Content/Libraries/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-						 "~/Scripts/bootstrap.js",
-						 "~/Scripts/respond.js"));
+						 "~/Content/Libraries/Bootstrap/bootstrap.js",
+						 "~/Content/Libraries/respond.js"));
 
-			bundles.Add(new StyleBundle("~/Content/css").Include(
-						 "~/Content/bootstrap.css",
-						 "~/Content/Site_CSS/*.css"));
+			bundles.Add(new StyleBundle("~/bundles/CSS").Include(
+						 "~/Content/Libraries/Bootstrap/bootstrap.css",
+						 "~/Content/CSS/*.css"));
 		}
 	}
 }
