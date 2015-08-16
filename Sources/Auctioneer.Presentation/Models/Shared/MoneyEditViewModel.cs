0bfde1f4c2c86a3c5718359eb380b87401cb468d
Patryk Strach <P.Strachu@gmail.com>
2015-08-16 20:11:45 +0200

    FIX: Server side validation of money's amount is missing

--- Sources/Auctioneer.Presentation/Models/Shared/MoneyEditViewModel.cs
+++ Sources/Auctioneer.Presentation/Models/Shared/MoneyEditViewModel.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 using Auctioneer.Logic.Currencies;
 
+using DataAnnotationsExtensions;
+
 namespace Auctioneer.Presentation.Models.Shared
 {
 	public class MoneyEditViewModel
 	{
+		[Required]
+		[Min(1.0)]
 		public decimal Amount { get; set; }
+
+		[Required]
 		public Currency Currency { get; set; }
 		public IEnumerable<SelectListItem> AvailableCurrencies { get; set; }
 	}
