bd39c2ea2938ca7a5dd1ca8e4f24bc3910b05374
Patryk Strach <P.Strachu@gmail.com>
2015-10-07 18:01:19 +0200

    Dynamically hide the inputs for minimum bidding price and buyout price when they are not enabled at "Add auction" page.

---
+++ Sources/Auctioneer.Presentation/Scripts/FormGroupEnabling.js
@@ -0,0 +1,31 @@
+﻿/*
+	A script for checkboxes controlling whether a given element is visible.
+	To use add "data-visibility-control-for = {selector}" to a checkbox to specify what element should be controlled.
+*/
+
+$(function()
+{
+	function getControlledElement(checkbox)
+	{
+		return $(checkbox).attr(attributeName);
+	}
+
+	var attributeName   = "data-visibility-control-for";
+	var checkBoxes      = $("[" + attributeName + "]");
+	var controlledItems = checkBoxes.map(function(i, element) { return getControlledElement(element); });
+
+	controlledItems.each(function(i, element)
+	{
+		$(element).hide();
+	});
+
+	checkBoxes.on("change", function()
+	{
+		var $controlledElement = $(getControlledElement(this));
+		if (this.checked) {
+			$controlledElement.slideDown(300);
+		} else {
+			$controlledElement.slideUp(300);
+		}
+	});
+})
